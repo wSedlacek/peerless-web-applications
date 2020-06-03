a59e4c8dd5bc2b4251ce9f31727f61e79dac5855
William Sedlacek <wsedlacekc@gmail.com>
2020-06-03 12:43:21 -0500

    style: Setup Material Styling
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- apps/drugs/src/app/app.module.ts
+++ apps/drugs/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabled' })],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
